9278b11883741c02c1650d832c67a5411a417ea9
menorton15 <menorton15@hotmail.com>
2022-01-07 19:21:22 -0700

    Update main.js
    added missing semicolons

--- WDD330/js/main.js
+++ WDD330/js/main.js
@@ -3,10 +3,10 @@ const links = [
         lable: "Week 1",
         url: "Week 01/index.html"
     }
-]
+];
 
-document.getElementById("table-of-contents").appendChild("<p> lskdjfllkdsfjlkdsjflskdfjlskdfjls</p>")
+document.getElementById("table-of-contents").appendChild("<p> lskdjfllkdsfjlkdsjflskdfjlskdfjls</p>");
 
 links.forEach( function addPageLink(item){
-    document.getElementById("table-of-contents").appendChild(`<li><a href=${item.url}>${item.lable}</a></li>`)   
-})
+    document.getElementById("table-of-contents").appendChild(`<li><a href=${item.url}>${item.lable}</a></li>`);   
+});
