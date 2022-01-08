4b1e9ef86cec14932e1b99474589abc3b16e82f8
menorton15 <menorton15@hotmail.com>
2022-01-07 20:19:26 -0700

    Update main.js
    modified again

--- WDD330/js/main.js
+++ WDD330/js/main.js
@@ -8,5 +8,7 @@ const links = [
 document.getElementById("table-of-contents").innerHTML="<li>alkhfjlksahfdjlakdfjhaldjhlfahj</li>";   
 
 links.forEach( function addPageLink(item){
-    document.getElementById("table-of-contents").innerHTML.concat(`<li><a href=${item.url}>${item.lable}</a></li>`);   
+    var element = document.createElement("li")
+    element.innerHTML = `<li><a href=${item.url}>${item.lable}</a></li>`
+    document.getElementById("table-of-contents").appendChild(element);   
 });
