c78a41a698a39125b6be21417356c20f1b087e41
menorton15 <menorton15@hotmail.com>
2022-01-07 19:59:22 -0700

    Update main.js
    Added test code back with modifications

--- WDD330/js/main.js
+++ WDD330/js/main.js
@@ -5,6 +5,8 @@ const links = [
     }
 ];
 
+document.getElementById("table-of-contents").innerHTML.concat(`<li><a href=${links[0].url}>${links[0].lable}</a></li>`);   
+
 links.forEach( function addPageLink(item){
     document.getElementById("table-of-contents").innerHTML.concat(`<li><a href=${item.url}>${item.lable}</a></li>`);   
 });
