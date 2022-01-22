78bebfdfb8f20efaa9bc669895204f6a1bb62857
menorton15 <menorton15@hotmail.com>
2022-01-22 13:47:15 -0700

    Added week 2 projects

---
+++ WDD330/Week02/Chapter2/main.js
@@ -0,0 +1,11 @@
+const quiz = [
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonder Woman's real name?","Diana Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+];
+
+const question = "What is Superman's real name?"
+const answer = prompt(question);
+let score = 0;
+
+alert(`You answered ${answer}`);
