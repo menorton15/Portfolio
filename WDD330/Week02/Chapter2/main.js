df671b263ee8021fc08e2991d9a42dc268bbc589
menorton15 <menorton15@hotmail.com>
2022-01-22 14:02:20 -0700

    Fixed links in chapter 2 and chapter 3

--- WDD330/Week02/Chapter2/main.js
+++ WDD330/Week02/Chapter2/main.js
@@ -1,11 +1,4 @@
-const quiz = [
-    ["What is Superman's real name?","Clark Kent"],
-    ["What is Wonder Woman's real name?","Diana Prince"],
-    ["What is Batman's real name?","Bruce Wayne"]
-];
-
 const question = "What is Superman's real name?"
 const answer = prompt(question);
-let score = 0;
 
 alert(`You answered ${answer}`);
